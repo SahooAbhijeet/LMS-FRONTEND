8cb589a7bab2af6f50560770f2ea134ce852488c
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-11-25 01:52:07 +0530

    added login signup buttons to sidebars

--- src/Layout/HomePageLayout.jsx
+++ src/Layout/HomePageLayout.jsx
@@ -1,9 +1,19 @@
 import { IoMenu } from "react-icons/io5";
 import { AiFillCloseCircle } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
+import { useDispatch, useSelector } from "react-redux";
 
 const HomeLayout = ({ children }) => {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    
+    //for checking if user is logged in
+    const isLoggedIn = useSelector((state) => state?.auth?.isLoggedIn);
+
+    //for displaying your actions according to role
+    const role =  useSelector((state) => state?.auth?.role);
 
     function changeWidth() {
         const drawerSide = document.getElementsByClassName("drawer-side");
@@ -15,8 +25,13 @@ const HomeLayout = ({ children }) => {
         element[0].checked = false;
 
         const drawerSide = document.getElementsByClassName("drawer-side");
-        drawerSide[0].style.width = 0
-        
+        drawerSide[0].style.width = 0        
+    }
+
+    function handleLogOut(e) {
+        e.preventDefault();
+
+        navigate("/");
     }
 
     return (
@@ -43,9 +58,15 @@ const HomeLayout = ({ children }) => {
                                 />
                             </button>
                         </li>
+
                         <li>
                             <Link to="/"> Home </Link>
                         </li>
+                         {isLoggedIn && role == 'ADMIN' && (
+                            <li>
+                                <Link to="/admin/dashboard"> ADMIN DASHBOARD </Link>
+                            </li>
+                         )}
                         <li>
                             <Link to="/courses"> All Courses </Link>
                         </li>
@@ -55,9 +76,31 @@ const HomeLayout = ({ children }) => {
                         <li>
                             <Link to="/about"> About Us </Link>
                         </li>
-                        <li>
-                            <Link to="/">Home</Link>
-                        </li>
+                        {!isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                            <div className="w-full flex items-center justify-center">
+                                <button className="bg-purple-800 btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/login"> Login</Link>
+                                </button>
+                                <button className="bg-pink-600 btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/login"> Signup </Link>
+                                </button>
+                            </div>
+                            </li>
+                        )}
+
+                            {isLoggedIn && (
+                            <li className="absolute bottom-4 w-[90%]">
+                            <div className="w-full flex items-center justify-center">
+                                <button className="bg-purple-800 btn-primary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link to="/user/profile"> Profile </Link>
+                                </button>
+                                <button className="bg-pink-600 btn-secondary px-4 py-1 font-semibold rounded-md w-full">
+                                    <Link onClick={handleLogOut}> LogOut </Link>
+                                </button>
+                            </div>
+                            </li>
+                        )}
                     </ul>
                 </div>
 
